261c061b0de54d064ea8062344cb2c6284c48578
Dimitris Menemenlis <menemenlis@jpl.nasa.gov>
2019-04-14 16:25:03 -0700

    adding SST diagnostics

--- matlab/lookat_ll1815.m
+++ matlab/lookat_ll1815.m
@@ -8,21 +8,45 @@ YC=readbin([pin 'YC.data'],[nx ny]);
 lon=XC(:,1)+360;
 lat=YC(1,:);
 
-% plot surface speed
+% extract/plot surface speed
 pin='/nobackup/dmenemen/NA/MITgcm/run/diags/';
+pout=[pin 'SSpeed/'];
+eval(['mkdir ' pout])
 fnm=dir([pin 'interior*data']);
+tmp=zeros(nx+1,ny+1);
 for m=1:length(fnm)
-    U=readbin([pin fnm(m).name],[nx ny],1,'real*4',nz*2);
-    V=readbin([pin fnm(m).name],[nx ny],1,'real*4',nz*3);
+    tmp(1:nx,1:ny)=readbin([pin fnm(m).name],[nx ny],1,'real*4',nz*2);
+    tmp(end,:)=tmp(nx,:);
+    U=(tmp(1:nx,1:ny)+tmp(2:end,1:ny))/2;
+    tmp(1:nx,1:ny)=readbin([pin fnm(m).name],[nx ny],1,'real*4',nz*3);
+    tmp(:,end)=tmp(:,ny);
+    V=(tmp(1:nx,1:ny)+tmp(1:nx,2:end))/2;
     speed=sqrt(U.^2+V.^2);
-    speed(find(~speed))=nan;
-    %    speed=log10(speed);
-    clf
+    eval(['writebin(''' pout 'SurfaceSpeedMonth' myint2str(m,3) ''',speed);'])
+    clf reset
     subplot(211)
+    speed(find(~speed))=nan;
     pcolorcen(lon,lat,speed');
     colormap(pink)
     caxis([0 1])
     colorbar('Position',[0.91    0.5841    0.015    0.3418])
-    title(['Month ' int2str(m) ' mean surface speed (m/s)'])
-    eval(['print -djpeg SurfaceSpeedMonth' myint2str(m)])
+    title(['Month ' myint2str(m,3) ' mean surface speed (m/s)'])
+    eval(['print -djpeg ' pout 'SurfaceSpeedMonth' myint2str(m,3)])
+end
+
+% extract/plot SST
+pout=[pin 'SST/'];
+eval(['mkdir ' pout])
+for m=1:length(fnm)
+    SST=readbin([pin fnm(m).name],[nx ny]);
+    eval(['writebin(''' pout 'SST_Month' myint2str(m,3) ''',SST);'])
+    clf reset
+    subplot(211)
+    SST(find(~SST))=nan;
+    pcolorcen(lon,lat,SST');
+    colormap(jet)
+    caxis([0 30])
+    colorbar('Position',[0.91    0.5841    0.015    0.3418])
+    title(['Month ' myint2str(m,3) ' SST (^oC)'])
+    eval(['print -djpeg ' pout 'SST_Month' myint2str(m,3)])
 end
